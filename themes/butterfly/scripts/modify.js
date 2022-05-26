173dccdd7e46c31845450d8051eca0bb431127e0
xilej <75837117+xilej@users.noreply.github.com>
2022-05-26 21:58:13 +0800

    Update modify.js

--- themes/butterfly/scripts/modify.js
+++ themes/butterfly/scripts/modify.js
@@ -6,6 +6,6 @@ filter.register('before_generate', () => {
     const { asset } = hexo.theme.config;
     for (const name in asset) {
         asset[name] = asset[name]
-            .replace('//cdn.tianli0.top/');
+            .replace('//jsdelivr.net/','//cdn.tianli0.top/');
     }
 }, 11);
