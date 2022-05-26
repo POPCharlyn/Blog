9abaf1de9b7640792f78656aa04cabb91960f7f4
xilej <75837117+xilej@users.noreply.github.com>
2022-05-26 22:03:05 +0800

    Create modify.js

---
+++ themes/butterfly/scripts/modify.js
@@ -0,0 +1,11 @@
+'use strict';
+const { filter } = hexo.extend;
+
+// 替换 CDN
+filter.register('before_generate', () => {
+    const { asset } = hexo.theme.config;
+    for (const name in asset) {
+        asset[name] = asset[name]
+            .replace('//cdn.jsdelivr.net/npm/', '//cdn1.tianli0.top/');
+    }
+}, 11);
